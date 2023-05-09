a4f4884aa2256c3b8f0fc45d0bcd03306d950522
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-09 18:18:22 -0300

    multiple parameters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     //println!("Hello, world!");
-    another_function(5);
+    //another_function(5);
+    print_labeled_measurement(5,'h');
 }
 
 fn another_function(x: i32){
     println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char){
+    print!("The measurement is: {value}{unit_label}");
 }
