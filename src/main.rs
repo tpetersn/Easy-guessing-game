544e29dde0cb3da705f25f8ebcff50abbeb8ecdc
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-09 19:25:57 -0300

    counting down using range and reverse with comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     for number in (1..4).rev() {
+        //(1..4) is called 'Range" and is provided by the standard library
+        //range generates all numbers in the sequence from one number and ending before another number
+        // .rev() reverses the range, counting from 3->2->1
         println!("{number}");
     }
     println!("LIFTOFF!!!");
