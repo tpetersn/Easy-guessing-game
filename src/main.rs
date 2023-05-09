32cacfc84b62127f68150a3f0e0c30e5b75407cd
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-09 19:00:14 -0300

    now with char

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let condition = true;
-    let number = if condition {"five"} else {"six"};
+    let number = if condition {"f"} else {"s"};
 
     print!("the value of the number is {number}");
 }
