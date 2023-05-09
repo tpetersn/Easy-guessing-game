0569fe9daebfd02ebdb6e1d97eaf0ec914ae3974
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-09 18:41:13 -0300

    another function with return values

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-fn five() -> i32 {
-    5
+fn main() {
+    let x = plus_one(5);
+    print!("the value of x is; {x}");
 }
 
-fn main() {
-    let x = five();
-    print!("The value of x is: {x}");
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
