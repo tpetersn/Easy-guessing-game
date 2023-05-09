9f097e4ca541feda1ed388f3f67371426b0b12e3
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-09 19:06:52 -0300

    returning values from loops

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    loop {
-        print!("again!!!");
-    }
+    let mut count = 0;
+
+    let result = loop {
+
+        count +=1;
+
+        if count == 10 {
+            break count * 2;
+        }
+    };
+
+    print!("the result is: {result}");
 }
