7196f623c1acbe90baaa0dc8bb1f73517b4c745c
Thomas Petersen <tpetersen1525@gmail.com>
2023-05-10 10:42:52 -0300

    final code for conversion between fahrenheit and celsius

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use std::io;
 
 fn main() {
-    println!("Enter a temperature in F to convert to C");
+    println!("Enter 1 to convert from F to C. Enter 2 to convert from C to F");
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("failed to read line");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).expect("failed to read input");
+    let user_input: u32 = user_input.trim().parse().expect("enter a number");
 
-    let input = input.trim().parse::<f32>().unwrap();
-    let converted_input: f32 = (input - 32.0) * 5.0/9.0;
-    println!("{input} degrees fahrenheit is {converted_input} in degrees celsius");
+
+
+
+
+    if user_input == 1{
+        println!("Enter a temperature in F to convert to C");
     
+        let mut inputf = String::new();
+        io::stdin().read_line(&mut inputf).expect("failed to read line");
+
+        let inputf = inputf.trim().parse::<f32>().unwrap();
+        let converted_inputf: f32 = (inputf - 32.0) * 5.0/9.0;
+        println!("{inputf} degrees fahrenheit is {converted_inputf} in degrees celsius");
+    } else {
+        println!("Enter a temperature in C to convert to F");
+    
+        let mut inputc = String::new();
+        io::stdin().read_line(&mut inputc).expect("failed to read line");
+
+        let inputc = inputc.trim().parse::<f32>().unwrap();
+        let converted_inputc: f32 = (inputc * 1.8) + 32.0;
+        println!("{inputc} degrees celsius is {converted_inputc} in degrees fahrenheit");
+
+
+    }
 }
